bc6b2289ea72a52b839c5deb91df0737eb330b2e
Magnus-Harder <magnusharder@gmail.com>
2023-03-04 17:16:26 +0100

    Scripts for Loading Data in Batches and Validation class

--- Utils.py
+++ Utils.py
@@ -11,28 +11,34 @@ import torch as th
 import numpy as np
 
 
+class ValidateModel:
 
+    def __init__(self, data_loader, data, batch_size, metrics, device = 'cpu',train=False):
+        self.data_loader = data_loader(data, batch_size, train = train, device = device, shuffle = False)
+        self.metrics = metrics
+        self.device = device
+        self.Softmax = th.nn.Softmax(dim=1)
 
-def get_metrics(y_true,y_pred,metric):
-    if metric == 'auc':
-        return roc_auc_score(y_true,y_pred)
+    def mean_reciprocal_rank(self, y_true, y_score):
+        order = th.argsort(y_score)[::-1]
+        y_true = th.take(y_true, order)
+        rr_score = y_true / (th.arange(len(y_true)) + 1)
+        return rr_score.sum()/ y_true.sum()
     
-
-    elif metric == 'logloss':
-        return log_loss(y_true,y_pred)
-    elif metric == 'mse':
-        return mean_squared_error(y_true,y_pred)
-    elif metric == 'accuracy':
-        return accuracy_score(y_true,y_pred)
-    elif metric == 'f1':
-        return f1_score(y_true,y_pred)
-    else:
-        raise ValueError('Unknown metric: {}'.format(metric))
+    def ROC_AUC(self, y_true, y_score):
+        return roc_auc_score(y_true, y_score, multi_class='ovo', labels = np.arange(0,295))
     
+    def get_metrics(self,model,batches):
+        
+        auc_score = 0
 
-def mrr(y_true, y_score):
-    order = np.argsort(y_score)[::-1]
-    y_true = np.take(y_true, order)
-    rr_score = y_true / (np.arange(len(y_true)) + 1)
-    return np.sum(rr_score) / np.sum(y_true)
-     
+        for i in range(batches):
+            User_en, Category, Subcategory, History_tensor, history_len, Category_Impressions, Subcategory_Impressions, Impressions_tensor, Impressions_len, Clicked = self.data_loader.__next__()
+
+            output = model(User_en, Category, Subcategory, History_tensor, history_len, Category_Impressions, Subcategory_Impressions, Impressions_tensor)
+            pred = self.Softmax(output)
+            
+            auc_score += self.ROC_AUC(Clicked.detach(),pred.detach())
+            print(self.ROC_AUC(Clicked.detach(),pred.detach()))
+
+        return auc_score/batches
