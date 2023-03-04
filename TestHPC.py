af98e9afb5e9abaf95ccb210a03c7dfee1e74cc0
Magnus-Harder <magnusharder@gmail.com>
2023-03-04 21:42:57 +0100

    HPC setup test

---
+++ TestHPC.py
@@ -0,0 +1,12 @@
+import torch as th
+import torchtext
+import sklearn
+import numpy as np
+import pandas as pd
+import random
+
+print("Hello HPC")
+
+
+list_test = list(1,2,3,4,5,6,7,8,9,10)
+
--- Utils.py
+++ Utils.py
@@ -11,6 +11,8 @@ import torch as th
 import numpy as np
 
 
+
+
 class ValidateModel:
 
     def __init__(self, data_loader, data, batch_size, metrics, device = 'cpu',train=False):
@@ -19,18 +21,24 @@ class ValidateModel:
         self.device = device
         self.Softmax = th.nn.Softmax(dim=1)
 
-    def mean_reciprocal_rank(self, y_true, y_score):
-        order = th.argsort(y_score)[::-1]
-        y_true = th.take(y_true, order)
-        rr_score = y_true / (th.arange(len(y_true)) + 1)
-        return rr_score.sum()/ y_true.sum()
-    
+    def mean_reciprocal_rank(self,y_true, y_score):
+        N,n_classes = y_score.shape
+        order = th.topk(y_score,k=n_classes).indices
+        rank = th.take(order,y_true) +1
+        rr_score = 1 / rank
+        print(rr_score)
+        return rr_score.sum()/ N
+        
     def ROC_AUC(self, y_true, y_score):
         return roc_auc_score(y_true, y_score, multi_class='ovo', labels = np.arange(0,295))
     
     def get_metrics(self,model,batches):
+
+        loss_fn = th.nn.CrossEntropyLoss()
         
         auc_score = 0
+        MRR_score = 0
+        loss_score = 0
         with th.no_grad():
             for i in range(batches):
                 User_en, Category, Subcategory, History_tensor, history_len, Category_Impressions, Subcategory_Impressions, Impressions_tensor, Impressions_len, Clicked = self.data_loader.__next__()
@@ -38,7 +46,17 @@ class ValidateModel:
                 output = model(User_en, Category, Subcategory, History_tensor, history_len, Category_Impressions, Subcategory_Impressions, Impressions_tensor)
                 pred = self.Softmax(output)
                 
-                auc_score += self.ROC_AUC(Clicked.detach().cpu(),pred.detach().cpu())
-                print(self.ROC_AUC(Clicked.detach().cpu(),pred.detach().cpu()))
 
-        return auc_score/batches
+                MRR = self.mean_reciprocal_rank(Clicked,pred).item()
+                AUC = self.ROC_AUC(Clicked.detach().cpu(),pred.detach().cpu())
+                loss = loss_fn(output, Clicked)
+                
+
+                auc_score += AUC
+                MRR_score += MRR
+                loss_score += loss.item()
+
+                print(f"Matrics Batch: AUC: {AUC} MRR: {MRR}")
+
+
+        return auc_score/batches , MRR_score/batches, loss_score/batches
