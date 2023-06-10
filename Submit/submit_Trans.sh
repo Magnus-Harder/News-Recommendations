e4214ea3a47bbe70e9dfeca1065d0ee87240b019
Magnus-Harder <magnusharder@gmail.com>
2023-06-10 15:13:06 +0200

    Dual Job Script CTNR-FInal Demo

--- Submit/submit_Trans.sh
+++ Submit/submit_Trans.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 #BSUB -J Trans
-#BSUB -o Trans_%J.out
-#BSUB -e Trans_%J.err
-#BSUB -q gpuv100
-#BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -n 1
-#BSUB -R "rusage[mem=16G]"
+#BSUB -o CTNR_%J.out
+#BSUB -e CTNR_%J.err
+#BSUB -q gpua100
+#BSUB -gpu "num=1:mode=exclusive_process:mps=yes"
+#BSUB -n 4
+#BSUB -R "rusage[mem=32G]"
 #BSUB -R "span[hosts=1]"
-#BSUB -W 24:00
+#BSUB -W 72:00
 #BSUB -N
+#BSUB -B
 # end of BSUB options
 
 # module load scipy/VERSION
@@ -22,4 +23,5 @@ module load cuda/11.7
 # NOTE: needs to have been built with the same SciPy version above!
 source venv_1/bin/activate
 
-python3 TrainTransformer/FitFinalTransformer.py
+python3 TrainTransformer/FitFinalTransformer.py & 
+python3 TrainTransformer/FitFinalTransformerOwn.py &
